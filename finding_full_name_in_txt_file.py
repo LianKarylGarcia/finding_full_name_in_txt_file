17a5814c4a59e4b2954d829e96c2bc3395f5c53c
LianKarylGarcia <liankarylg@gmail.com>
2024-11-30 09:11:13 -0500

    Codes for extracting the name in the txt file

--- finding_full_name_in_txt_file.py
+++ finding_full_name_in_txt_file.py
@@ -1,2 +1,13 @@
 def find_user_info(full_name_to_find): # Define the function find_user_info
-    with open("user_infos.txt", "r") as file: # to open and read the file in prog 1
+    
+    try:
+        with open("user_infos.txt", "r") as file: # to open and read the file in prog 1
+            found = False # To track if the user is in the file
+            for line in file: # Loop
+                if line.strip().startswith("Full Name: "): # Make sure line starts with "Full Name:"
+                    print("User found: ")
+                elif line.strip()[11:] == full_name_to_find: # Extract the name
+                    print(line.strip())
+        
+    except:
+
